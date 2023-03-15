f920755a372bb24f573de9e77cfbc221adfa7ef7
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-03-15 12:49:17 +0100

    Ejercicio 1 Creacion

---
+++ Ejercicio3/src/ejercicio1/mainApp1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ejercicio1;
+
+/**
+ * @author aitor
+ *
+ */
+public class mainApp1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
