55f9faac204fa38300d8a568b28842e4bfd92c80
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-03-15 13:22:31 +0100

    Ejercicio 2 creado

---
+++ Ejercicio3/src/ejercicio2/Serie.java
@@ -0,0 +1,83 @@
+package ejercicio2;
+
+public class Serie {
+	// CONSTANTES
+	static final int temp = 3;
+	static final boolean ent = false;
+
+	// ATRIBUTOS
+	private String titulo;
+	private int numeroTemporadas;
+	private boolean entregado;
+	private String genero;
+	private String creador;
+
+	// CONSTRUCTORES
+	// 1-Constructor por defecto
+	public Serie() {
+		this.titulo = "";
+		this.numeroTemporadas = temp;
+		this.entregado = ent;
+		this.genero = "";
+		this.creador = "";
+	}
+
+	// 2- Constructor con titulo y creador
+	public Serie(String titulo, String creador) {
+		this.titulo = titulo;
+		this.numeroTemporadas = temp;
+		this.entregado = ent;
+		this.genero = "";
+		this.creador = creador;
+	}
+
+	// 3- Constructor con todo excepto entregado
+	public Serie(String titulo, int temporadas, String genero, String creador) {
+		this.titulo = titulo;
+		this.numeroTemporadas = temporadas;
+		this.entregado = ent;
+		this.genero = genero;
+		this.creador = creador;
+	}
+
+	// GETTERS y SETTERS
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public int getNumeroTemporadas() {
+		return numeroTemporadas;
+	}
+
+	public void setNumeroTemporadas(int numeroTemporadas) {
+		this.numeroTemporadas = numeroTemporadas;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getCreador() {
+		return creador;
+	}
+
+	public void setCreador(String creador) {
+		this.creador = creador;
+	}
+
+	// METODOS
+	@Override
+	public String toString() {
+		return "Serie [titulo=" + titulo + ", numeroTemporadas=" + numeroTemporadas + ", entregado=" + entregado
+				+ ", genero=" + genero + ", creador=" + creador + "]";
+	}
+
+}
---
+++ Ejercicio3/src/ejercicio2/mainApp2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ejercicio2;
+
+/**
+ * @author aitor
+ *
+ */
+public class mainApp2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
