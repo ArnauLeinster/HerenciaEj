5e851ef24b6a166206d26ed8aeb055816861dd94
MarcSaezA <72219572+MarcSaezA@users.noreply.github.com>
2023-03-16 12:47:44 +0100

    Comentarios + algunos ajustes del ejercicio 3

--- Ejercicio3/src/Libro.java
+++ Ejercicio3/src/Libro.java
@@ -1,6 +1,12 @@
 
 public class Libro {
-	private String ISBN;
+	private String ISBN="1340901217";
+	private String titulo=null;
+	private String autor=null;
+	private int numPag=30;
+	
+	//getters y setters 
+	
 	public String getISBN() {
 		return ISBN;
 	}
@@ -25,10 +31,8 @@ public class Libro {
 	public void setNumPag(int numPag) {
 		this.numPag = numPag;
 	}
-	private String titulo;
-	private String autor;
-	private int numPag;
 	
+	//default constructor
 	public Libro()
 	{
 		this.ISBN="1340901217";
@@ -36,16 +40,19 @@ public class Libro {
 		this.autor=null;
 		this.numPag=30;
 	}
+	//constructor that ask for ISBN 
 	public Libro(String ISBN_new)
 	{
 		this.ISBN=ISBN_new;
 	}
+	//constructor that have almost everything
 	public Libro(String title,String author,int nuPag)
 	{
 		this.titulo=title;
 		this.autor=author;
 		this.numPag=nuPag;
 	}
+	//toString to show the message
 	public String toString()
 	{
 		return ("El libro "+this.titulo+" con autor "+this.autor+" que tiene "+this.numPag+" paginas, tiene el ISBN de "+ this.ISBN);
