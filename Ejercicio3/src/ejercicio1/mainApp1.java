b32fe29f906fa640b5ba2e91e44bfe6b1d2cb7a4
MarcSaezA <72219572+MarcSaezA@users.noreply.github.com>
2023-03-16 12:01:49 +0100

    Ejercicio 1 completado

--- Ejercicio3/src/ejercicio1/mainApp1.java
+++ Ejercicio3/src/ejercicio1/mainApp1.java
@@ -3,6 +3,9 @@
  */
 package ejercicio1;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * @author aitor
  *
@@ -14,13 +17,54 @@ public class mainApp1 {
 	 */
 	public static void main(String[] args) {
 
-			Electrodomestico tt1=new  Electrodomestico();
-			Electrodomestico tt2=new  Electrodomestico(34, 20.4);
-			Electrodomestico tt3=new  Electrodomestico(450.99,"Azul",'a',20.50);
-			System.out.println(tt1.getPrecioBase()+" "+tt1.getColor()+" "+tt1.getConsumoEnerg()+" "+tt1.getPeso());
-			System.out.println(tt2.getPrecioBase()+" "+tt2.getColor()+" "+tt2.getConsumoEnerg()+" "+tt2.getPeso());
-			System.out.println(tt3.getPrecioBase()+" "+tt3.getColor()+" "+tt3.getConsumoEnerg()+" "+tt3.getPeso());
+			ArrayList<Electrodomestico> compraEle= createArray();
+			seePrice(compraEle);
+			
+	}
+	public static ArrayList<Electrodomestico> createArray()
+	{
+		ArrayList<Electrodomestico> list= new ArrayList<Electrodomestico>();
+		list.add(new Television());
+		list.add(new Lavadora(20,31));
+		list.add(new Electrodomestico(76,"Azul",'a',180));
+		list.add(new Television());
+		list.add(new Television(180,10));
+		list.add(new Lavadora());
+		list.add(new Television(247,"Gris",'e',18,43.5,false));
+		list.add(new Lavadora(50,"Rojo",'b',70,29));
+		list.add(new Electrodomestico());
+		list.add(new Electrodomestico(48,12));
+		
+		return list;
+		
+	}
+	public static void seePrice(ArrayList<Electrodomestico> eleArr)
+	{
+		double precioEle=0;
+		double precioTele=0;
+		double precioLava=0;
+		double actualPrice=0;
+		Television tele=new Television();
+		Lavadora lav= new Lavadora();
+		
+		Iterator<Electrodomestico> eleIter= eleArr.iterator();
+		for(int i=0; i<10; i++)
+		{
+			Electrodomestico leTest=eleIter.next();
+			actualPrice=leTest.plusPrice();
+			System.out.println("El precio final del producto numero "+(i+1) +" es de "+actualPrice);
+			if((leTest.getClass()==tele.getClass())&&(leTest.getClass()!=lav.getClass()))
+			{
+				precioTele=precioTele+actualPrice;
+			
+			}
+			else if((leTest.getClass()!=tele.getClass())&&(leTest.getClass()==lav.getClass()))
+			{
+				precioLava=precioLava+actualPrice;
+			}
+			precioEle=precioEle+actualPrice;
 
-
+		}
+		System.out.println("Seran "+precioEle+" de todos los electrodomesticos "+precioLava+" por lavadoras y "+precioTele+" de televisores");
 	}
 }
